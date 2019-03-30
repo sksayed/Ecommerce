eeca9cb5964ed2096bd0e338094dea6583d8a0ac
Sheikh Sayed <sksayed@live.com>
2019-03-31 00:09:00 +0600

    estore admin mustakim mustakim , user sksayed sayed1122

--- estore.sql
+++ estore.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 18, 2018 at 10:18 AM
--- Server version: 10.1.24-MariaDB
--- PHP Version: 7.1.6
+-- Generation Time: Mar 10, 2019 at 03:37 AM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -48,7 +48,9 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`id`, `name`, `username`, `email`, `phone`, `password`, `gender`, `dob`, `nid`, `presentaddress`, `parmanentaddress`) VALUES
 (1, 'Noyon', 'shanto', 'shanto@gmail.com', '', 'sha1$ebb2bdc9$1$d77148ab8c6268c2f43b5fafdfa3f0405bd24306', 'male', '1996-05-24', '19979972342363842', 'H-23,R-4,Nikunja-2,Khilkhet,Dhaka', 'Dhaka'),
-(2, 'arnob', 'arnob', 'ar@gmIL.COM', '', 'sha1$f77072c1$1$ffafbbe013a168ccde2c4c3ee99cc322e05854c6', 'male', '9843-12-12', '3245465756543', 'wgbnb,hstr', 'geawaiuobvgair,gaer');
+(2, 'arnob', 'arnob', 'ar@gmIL.COM', '', 'sha1$f77072c1$1$ffafbbe013a168ccde2c4c3ee99cc322e05854c6', 'male', '9843-12-12', '3245465756543', 'wgbnb,hstr', 'geawaiuobvgair,gaer'),
+(4, 'mustakim', 'mustakim', 'mustakim@gmail.com', '', 'sha1$a648cd43$1$937222cc377b158cf56b32aa1285abb7ced755d8', 'male', '0000-00-00', '', '', ''),
+(6, 'sayed rahman', 'sksayed', 'sayedrahman015@gmail.com', '', 'sha1$b2fb9425$1$41ae88b3b6ee239f8c7e9562fce328c89bb6be35', 'male', '2019-03-19', '1325467498', 'bashundhara', 'bashundhara');
 
 -- --------------------------------------------------------
 
@@ -74,12 +76,14 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `productname`, `price`, `quantity`, `catagory`, `image1`, `image2`, `image3`, `details`, `date`) VALUES
-(2, 'Sony HD 4K Smart LED TV', 350, 1, 'Electronics', NULL, NULL, NULL, '', '0000-00-00'),
-(3, 'Biskut', 12, 200, 'Food', NULL, NULL, NULL, 'Lohar Biskut', '0000-00-00'),
-(6, 'Amazfit Bip smart watch', 4800, 1, 'Electronics', NULL, NULL, NULL, 'Watch', '0000-00-00'),
-(7, 'Touhid', 10, 1, 'Men', NULL, NULL, NULL, 'lsiddlsfuklh', '0000-00-00'),
-(8, 'Shanto\'s Ball', 2, 2, 'Men', NULL, NULL, NULL, 'Nice Soft Black Punchable', '0000-00-00'),
-(9, 'Watch', 200, 12, 'Electronics', NULL, NULL, NULL, 'Abc', '0000-00-00');
+(2, 'Sony HD 4K Smart LED TV', 350, 80, 'Electronics', NULL, NULL, NULL, '', '0000-00-00'),
+(3, 'Biskut', 12, 400, 'Food', NULL, NULL, NULL, '', '0000-00-00'),
+(6, 'Amazfit Bip smart watch', 4800, 500, 'Electronics', NULL, NULL, NULL, '', '0000-00-00'),
+(7, 'Touhid', 10, 480, 'Men', NULL, NULL, NULL, '', '0000-00-00'),
+(8, 'Shanto\'s Ball', 2, 790, 'Men', NULL, NULL, NULL, '', '0000-00-00'),
+(9, 'Watch', 200, 300, 'Electronics', NULL, NULL, NULL, '', '0000-00-00'),
+(10, 'Brush', 45, 780, 'food', NULL, NULL, NULL, '', '0000-00-00'),
+(11, 'money bag 2', 200, 600, 'accessories', NULL, NULL, NULL, '', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -122,7 +126,7 @@ INSERT INTO `soldproduct` (`id`, `productid`, `userid`, `quantity`, `price`, `ad
 (20, 7, 30, 1, '10', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (21, 3, 30, 100, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (22, 9, 30, 1, '200', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(23, 3, 30, 100, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
+(23, 3, 30, 100, '12', 0, '1229', 'yes', '2018-03-18 04:12:16'),
 (24, 9, 30, 1, '200', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (25, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (26, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
@@ -130,7 +134,7 @@ INSERT INTO `soldproduct` (`id`, `productid`, `userid`, `quantity`, `price`, `ad
 (28, 7, 30, 1, '10', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (29, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (30, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(31, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
+(31, 3, 30, 1, '12', 0, '1229', 'yes', '2018-03-18 04:12:16'),
 (32, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (33, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (34, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
@@ -139,7 +143,7 @@ INSERT INTO `soldproduct` (`id`, `productid`, `userid`, `quantity`, `price`, `ad
 (37, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (38, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (39, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(40, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
+(40, 3, 30, 1, '12', 0, '1229', 'yes', '2018-03-18 04:12:16'),
 (41, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (42, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (43, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
@@ -147,20 +151,20 @@ INSERT INTO `soldproduct` (`id`, `productid`, `userid`, `quantity`, `price`, `ad
 (45, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (46, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (47, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(48, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
+(48, 2, 30, 1, '350', 0, '1229', 'yes', '2018-03-18 04:12:16'),
 (49, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (50, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(51, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(52, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(53, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(54, 2, 30, 1, '3', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(55, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(56, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(57, 3, 30, 1, '1', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(58, 3, 30, 1, '1', 0, '1229', 'no', '2018-03-18 04:12:16'),
+(51, 6, 30, 1, '4800', 0, '1229', 'yes', '2018-03-18 04:12:16'),
+(52, 2, 30, 1, '350', 0, '1229', 'yes', '2018-03-18 04:12:16'),
+(53, 6, 30, 1, '4800', 0, '1229', 'yes', '2018-03-18 04:12:16'),
+(54, 2, 30, 1, '3', 0, '1229', 'yes', '2018-03-18 04:12:16'),
+(55, 2, 30, 1, '350', 0, '1229', 'yes', '2018-03-18 04:12:16'),
+(56, 6, 30, 1, '4800', 0, '1229', 'yes', '2018-03-18 04:12:16'),
+(57, 3, 30, 1, '1', 0, '1229', 'yes', '2018-03-18 04:12:16'),
+(58, 3, 30, 1, '1', 0, '1229', 'yes', '2018-03-18 04:12:16'),
 (59, 3, 30, 1, '1', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(60, 3, 30, 1, '1', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(61, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
+(60, 3, 30, 1, '1', 0, '1229', 'yes', '2018-03-18 04:12:16'),
+(61, 3, 30, 1, '12', 0, '1229', 'yes', '2018-03-18 04:12:16'),
 (62, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (63, 2, 30, 1, '350', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (64, 3, 30, 1, '12', 0, '1229', 'no', '2018-03-18 04:12:16'),
@@ -177,8 +181,8 @@ INSERT INTO `soldproduct` (`id`, `productid`, `userid`, `quantity`, `price`, `ad
 (75, 6, 30, 1, '4800', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (76, 2, 30, 1, '3', 0, '1229', 'no', '2018-03-18 04:12:16'),
 (77, 2, 30, 1, '3', 0, '1229', 'no', '2018-03-18 04:12:16'),
-(78, 2, 30, 1, '3', 0, '1229', 'no', '2018-03-18 04:14:08'),
-(79, 3, 30, 1, '1', 0, '1229', 'no', '2018-03-18 04:15:20');
+(78, 2, 30, 1, '3', 0, '1229', 'yes', '2018-03-18 04:14:08'),
+(79, 3, 30, 1, '1', 0, '1229', 'yes', '2018-03-18 04:15:20');
 
 -- --------------------------------------------------------
 
@@ -201,11 +205,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `username`, `email`, `password`, `phone`, `address`) VALUES
-(30, 'Md. Hasan Uzzaman', 'noyon', 'noyon892@gmail.com', 'sha1$57f1ff4a$1$ba87cb8290a3bafe3d283d25f4a33d22e681cb2f', '1733844422', 'Mahajon Bari,Affratpara,Chatmohar,Pabna'),
 (31, 'Md. Hasan Uzzaman', 'noyon892', 'noyon@gmail.com', 'sha1$3ea3ed64$1$43ed3d8b7e5f8c07163bc5e0c8c7d1005c77de2e', '01733844422', 'Mahajon Bari,Affratpara,Chatmohar,Pabna'),
-(32, 'Md. Hasan Uzzaman', 'noyon8920', 'noyon8920@gmail.com', 'sha1$56996c2c$1$a9abbf21a11ce77b4952a2e18d00bdbd27d3e605', '01733844422', 'Mahajon Bari,Affratpara,Chatmohar,Pabna'),
-(33, 'Souvik', 'souvik420', 'Souvik420@gmail.com', 'sha1$066daa5e$1$d341b8bf72a59ce10a256fc5d92a584a6a74ace4', '01768864040', 'Kahalu,Bogura'),
-(34, 'Md. Hasan Uzzaman', 'jame', 'jame@gmail.com', 'sha1$381f4664$1$81c2590f85491f6fb0275dfc3a065fc1b85e4ab7', '01733844422', 'Mahajon Bari,Affratpara,Chatmohar,Pabna');
+(35, 'sayed rahman', 'sksayed', 'sayedrahman015@gmail.com', 'sha1$ccf552bb$1$ad89690f700e6bd81f999d017a8a770a5ea95080', '01670420393', 'halishahar'),
+(36, 'sayedrahman3934', 'sayed', 'zahidneru@gmail.com', 'sha1$62063995$1$cebe5118aa22018cf8b96bd0df54142ebe5ba27e', '01670420393', 'halishahar b block');
 
 --
 -- Indexes for dumped tables
@@ -247,22 +249,26 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
 -- AUTO_INCREMENT for table `soldproduct`
 --
 ALTER TABLE `soldproduct`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
